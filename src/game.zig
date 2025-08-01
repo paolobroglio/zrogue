7cee516c55251683506a407f9a2d5901e67c7df8
Paolo Broglio <paolobroglio@pm.me>
2025-08-01 14:25:01 +0200

    wip

--- src/game.zig
+++ src/game.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const debug = std.debug;
 const as = @import("assetstore.zig");
+const rl = @import("raylib");
 
 pub const Error = enum {
   InitializationFailed, 
@@ -19,6 +20,8 @@ pub const Game = struct {
   highdpi: bool,
   fps: i32,
 
+  is_running: bool,
+
   asset_store: as.AssetStore,
 
   map_width: i32,
@@ -31,17 +34,34 @@ pub const Game = struct {
       .vsync = true,
       .highdpi = true,
       .fps = 60,
+      .is_running = false,
       .asset_store = as.AssetStore.init(allocator),
       .map_width = 80, // window width / tile_size
       .map_height = 50 // window height / tile_size
     };
   }
   pub fn deinit(self: *Game) Error!void {
+    rl.closeWindow();
     self.asset_store.deinit();
   }
-  pub fn run() Error!void {}
-
-  fn setup() Error!void {}
+  fn setup(self: *const Game) Error!void {
+    rl.setConfigFlags(.{
+      .window_highdpi = self.highdpi, 
+      .window_resizable = false, 
+      .vsync_hint = self.vsync
+    });
+    rl.initWindow(self.window_width, self.window_height, "ZRogue");
+    rl.setTargetFPS(self.fps);
+  }
+  pub fn run(self: *const Game) Error!void {
+    self.setup();
+    const execute_loop = !rl.windowShouldClose() and self.is_running;
+    while (execute_loop) {
+      self.processInput();
+      self.update();
+      self.render();
+    }
+  }
   fn processInput() Error!void {}
   fn update() Error!void {}
   fn render() Error!void {}
